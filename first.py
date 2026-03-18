1e5977bc026ee7a9db32cfa2fbc21d84a1584265
hngo13-cell <h_ngo13@u.pacific.edu>
2026-03-18 10:10:05 -0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 5, 4, 5]
+
+plt.figure()
+plt.scatter(x, y)
+
+plt.title("Scatter Plot Example")
+plt.xlabel("X")
+plt.ylabel("Y")
+
+plt.show()
